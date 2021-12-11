b76e22d561ba0810da1aacbc456a6ad3c676061d
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-12-11 10:42:55 -0600

    task promises written

---
+++ src/helpers/tasksData.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import firebaseConfig from '../api/apiKeys';
+
+const dbUrl = firebaseConfig.databaseURL;
+
+const getAllTasksByDog = (dogObj) => new Promise((resolve, reject) => {
+  axios
+    .get(`${dbUrl}/tasks.json?orderBy="dogId"&equalTo="${dogObj.dogId}"`)
+    .then((response) => resolve(Object.values(response.data)))
+    .catch(reject);
+});
+
+const getSingleTask = (taskFirebaseKey) => new Promise((resolve, reject) => {
+  axios
+    .get(`${dbUrl}/tasks/${taskFirebaseKey}.json`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+const createTask = (obj, dogObj) => new Promise((resolve, reject) => {
+  axios
+    .post(`${dbUrl}/tasks.json`, obj)
+    .then((response) => {
+      const taskFirebaseKey = { taskFirebaseKey: response.data.name };
+      axios
+        .patch(`${dbUrl}/tasks/${response.data.name}.json`, taskFirebaseKey)
+        .then(() => {
+          getAllTasksByDog(dogObj).then(resolve);
+        });
+    })
+    .catch(reject);
+});
+
+const updateTask = (taskObj, dogObj) => new Promise((resolve, reject) => {
+  axios
+    .patch(`${dbUrl}/tasks/${taskObj.taskFirebaseKey}.json`, taskObj)
+    .then(() => getAllTasksByDog(dogObj).then(resolve))
+    .catch(reject);
+});
+
+const deleteTask = (taskFirebaseKey, dogObj) => new Promise((resolve, reject) => {
+  axios
+    .delete(`${dbUrl}/tasks/${taskFirebaseKey}.json`)
+    .then(() => getAllTasksByDog(dogObj).then(resolve))
+    .catch(reject);
+});
+
+export {
+  getAllTasksByDog, getSingleTask, createTask, updateTask, deleteTask,
+};
