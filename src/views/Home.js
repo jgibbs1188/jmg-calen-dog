c1c73f83defdaeb9cd943d43972014ceeefe4e10
Joe Gibbs <joseph.gibbs1188@gmail.com>
2021-12-20 14:58:21 -0600

    updated Home page text

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,11 +4,10 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <>
-      <div>
-        Home Page! Here you will see a link to your dashboard and dogs.
-        <br />
-        Maybe even your profile!
-      </div>
+      <h3>
+        Welcome! Please use the links below to view your current list of dogs or
+        to create a new dog!
+      </h3>
       <br />
       <Link className="nav-link active" aria-current="page" to="/new">
         Create a New Doggo
